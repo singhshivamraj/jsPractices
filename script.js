cc9b9ac57c0d8abd95aa544bbfe1a1e1fe99080f
singhshivamraj <shivamraj6436@gmail.com>
2025-09-04 23:21:37 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+/*question(6) Print all numbers from 1 to 100. 
+
+for(let i = 0; i<= 100; i++ ){
+  console.log(i);
+  
+}*/
+
+
+/* question(2)Print all even numbers from 1 to 100. 
+
+for(let i = 0; i<= 100; i++){
+  if (i % 2 === 0) {
+   console.log(i);
+   
+    
+  }
+}*/
+
+
+/*  question(3)Print odd numbers from 1 to 100. 
+
+
+for(let i = 0; i<= 100; i++){
+  if (i % 2 !== 0) {
+    console.log(i);
+    
+  }
+}*/
+
+
+
+
+
+
+
+/* question(6) Find whether a number is prime or not. 
+
+let number = parseInt(prompt("enter a number"))
+
+
+if (number % 2 === 0) {
+  console.log('number is prime');
+  
+}else{
+  console.log('number is not prime');
+  
+}*/
